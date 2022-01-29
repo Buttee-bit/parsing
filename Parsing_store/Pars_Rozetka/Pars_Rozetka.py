c8fdc410b7bbd41d9569b36f4bcc6daac8fddfa2
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2022-01-29 19:54:35 +0300

    функция захода по хэштегу и лайкать первые (match), обратить внимание, что кириллица переводиться в HEX

---
+++ Parsing_store/Pars_Rozetka/Pars_Rozetka.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+
+
+
+
+def request(url,config):
+    r = requests.get(url,headers=config)
+    if r.status_code == 200:
+        return r.text
+    else:
+        print('Ошибка подключения')
+
+
+def config():
+    headers = {
+        'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    }
+    return headers
+
+def main():
+    headers = config()
+    url = "https://rozetka.com.ua/ua/"
+    print(request(url,headers))
+
+main()
