ac1694745a42b91ec44f9ab50b6f7e27f0516b09
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2022-01-30 14:34:26 +0300

    функция находящая число страниц + запись ссылок на страницы

--- Parsing_store/Pars_Rozetka/Pars_Rozetka.py
+++ Parsing_store/Pars_Rozetka/Pars_Rozetka.py
@@ -29,22 +29,40 @@ def get_list_category(requests_main_page_text):
     return list_category
 
 
-def get_list_category_next(index_link, header):
-    r = request(index_link, headers=header)
+def get_list_category_next(index_link, headers):
+    r = request(index_link, headers=headers)
     soup = BeautifulSoup(r,'lxml')
     list_links_category_next = soup.find_all(class_='tile-cats__picture')
     list_link = []
     for i in list_links_category_next[0:18]:
-        print(i)
         list_link.append(i.get('href'))
-    print(list_link)
+    return list_link
+
+
+def get_pagination_page_and_soup(page,header):
+    r = request(page,headers=header)
+    soup = BeautifulSoup(r,'lxml')
+    all_pagenation = soup.find_all(class_='pagination__link ng-star-inserted')[-1].text
+    return int(all_pagenation)
+
+
+def get_list_card(link_down_category,header):
+    pagenation = get_pagination_page_and_soup(link_down_category,header)
+    all_card = []
+    print(f'{link_down_category}')
+    for i in range(0,pagenation+1):
+        link = f'{link_down_category}page={i}/'
+
+
+    pass
 
 def main():
     headers = config()
     url = "https://rozetka.com.ua/ua/"
     list_links_category = get_list_category(request(url, headers))
-    print(list_links_category[0])
+    #print(list_links_category[0])
     list_links_category_next = get_list_category_next(list_links_category[0],headers)
-
+    print(list_links_category_next[0])
+    print(get_list_card(list_links_category_next[0], headers))
 
 main()
