bee884c9ebe2da98050afa124e7ffd023b18fe56
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2022-01-31 14:07:43 +0300

    Дописана функция находящая все ссылки на странице + Написаны комментарии , что б самому не забыть )

--- Parsing_store/Pars_Rozetka/Pars_Rozetka.py
+++ Parsing_store/Pars_Rozetka/Pars_Rozetka.py
@@ -3,7 +3,7 @@ import requests
 import csv
 
 
-def request(url, headers):
+def request(url, headers):  # дает код страницы
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
         return r.content
@@ -11,7 +11,7 @@ def request(url, headers):
         print('Ошибка подключения')
 
 
-def config():
+def config():  # конфигурация к реквест запросу
     headers = {
         'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
@@ -19,7 +19,7 @@ def config():
     return headers
 
 
-def get_list_category(requests_main_page_text):
+def get_list_category(requests_main_page_text):  # дает список ссылок на категории из шлавного меню
     soup = BeautifulSoup(requests_main_page_text, 'lxml')
 
     category = soup.find_all(class_='menu-categories__link')
@@ -29,9 +29,9 @@ def get_list_category(requests_main_page_text):
     return list_category
 
 
-def get_list_category_next(index_link, headers):
+def get_list_category_next(index_link, headers):  # Дает лист ссылок в ПОДкатегории
     r = request(index_link, headers=headers)
-    soup = BeautifulSoup(r,'lxml')
+    soup = BeautifulSoup(r, 'lxml')
     list_links_category_next = soup.find_all(class_='tile-cats__picture')
     list_link = []
     for i in list_links_category_next[0:18]:
@@ -39,30 +39,42 @@ def get_list_category_next(index_link, headers):
     return list_link
 
 
-def get_pagination_page_and_soup(page,header):
-    r = request(page,headers=header)
-    soup = BeautifulSoup(r,'lxml')
+def get_pagination_page_and_soup(page, headers):  # Дает интовое значение последней страницы
+    r = request(page, headers=headers)
+    soup = BeautifulSoup(r, 'lxml')
     all_pagenation = soup.find_all(class_='pagination__link ng-star-inserted')[-1].text
     return int(all_pagenation)
 
 
-def get_list_card(link_down_category,header):
-    pagenation = get_pagination_page_and_soup(link_down_category,header)
-    all_card = []
-    print(f'{link_down_category}')
-    for i in range(0,pagenation+1):
+def get_list_page_card(link_down_category, headers):  # с i'той страницы собирает ссылки на все карторчки товара
+    pagenation = get_pagination_page_and_soup(link_down_category, headers)
+    list_page_card = []
+    # print(f'{link_down_category}')
+    for i in range(1, pagenation + 1):
         link = f'{link_down_category}page={i}/'
+        list_page_card.append(link)
+    return list_page_card
 
 
-    pass
+def get_open_card(list_page_card, headers):
+    r = request(list_page_card, headers=headers)
+    soup = BeautifulSoup(r, 'lxml')
+    all_card = soup.find_all(class_='goods-tile__picture ng-star-inserted')
+    list_card_link = []
+    for i in all_card:
+        list_card_link.append(i.get('href'))
+    return list_card_link
+
 
 def main():
     headers = config()
     url = "https://rozetka.com.ua/ua/"
     list_links_category = get_list_category(request(url, headers))
-    #print(list_links_category[0])
-    list_links_category_next = get_list_category_next(list_links_category[0],headers)
-    print(list_links_category_next[0])
-    print(get_list_card(list_links_category_next[0], headers))
+    # print(list_links_category[0])
+    list_links_category_next = get_list_category_next(list_links_category[0], headers)
+    # print(list_links_category_next[0])
+    list_page_card = get_list_page_card(list_links_category_next[0], headers)[0]
+    list_card_link = get_open_card(list_page_card, headers)
+
 
 main()
